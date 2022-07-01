23b71178822642bf7842cb6e54fbe431ef959d7a
Timothy Gaither <timjgaither@gmail.com>
2022-07-01 16:58:12 +0200

    Rifatta parte vista in classe

---
+++ Banca.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_banca_oop
+{
+    internal class Banca
+    {
+        public string Nome { get; set; }   
+        List<Cliente> clienti;
+        List<Prestito> prestiti;
+
+        public Banca (string nome)
+        {
+            this.Nome = nome;
+            clienti = new List<Cliente> (); 
+            prestiti = new List<Prestito> ();   
+        }
+
+
+        public void StampaNomeBanca()
+        {
+             Console.WriteLine("\t Benvenuto in "+this.Nome);
+        }
+        
+
+        public void NuovoCliente(Cliente cliente)
+        {
+            clienti.Add(cliente);
+        }
+
+       
+
+        public static Cliente CreaCliente()
+        {
+            Banca.StampaTitolo("Creazione nuovo cliente", "Inserisci i dati cliente.");
+          
+            Console.WriteLine("\t Inserisci il nome ");
+            string nome= Console.ReadLine();
+            Console.WriteLine("\t Inserisci il cognome ");
+            string cognome = Console.ReadLine();
+            Console.WriteLine("\t Inserisci il Codice Fiscale ");
+            string codiceFiscale = Console.ReadLine();
+            
+            Cliente nuovo = new Cliente(nome, cognome, codiceFiscale);
+           
+            return nuovo;
+        }
+       
+        internal void StampaListaClienti()
+        {
+            Banca.StampaTitolo("Lista clienti","");
+            int pos = 1;
+            foreach (Cliente cliente in clienti)
+            {
+                Console.WriteLine("\t"+pos + ")");
+                cliente.Stampa();
+                pos++;
+            }   
+        }
+
+        public static  void StampaTitolo(string titolo, string sottotitolo)
+        {
+            
+            Console.WriteLine("\t****{0}****", titolo);
+            Console.WriteLine();
+            Console.WriteLine("\t"+"**"+sottotitolo+"**");
+            Console.WriteLine();
+        }
+        internal static int RichiediCliente()
+        {
+            Console.WriteLine("\t Inserisci codice cliente: ");
+            int numCliente = int.Parse(Console.ReadLine());
+            return numCliente;
+        }
+
+
+        
+    }
+}
---
+++ Cliente.cs
@@ -0,0 +1,22 @@
+﻿namespace csharp_banca_oop
+{
+    internal class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string CodiceFiscale { get; set; }
+        public int Stipendio { get; set; }
+
+        public Cliente(string nome,string Cognome, string CodiceFiscale)
+        {
+            this.Nome = nome;
+            this.Cognome = Cognome;
+            this.CodiceFiscale = CodiceFiscale;
+        }
+        internal void Stampa()
+        {
+          
+            Console.WriteLine($"\t Nome : {this.Nome} \t Cognome : {this.Cognome} \t Codice Fiscale : {this.CodiceFiscale}");
+        }
+    }
+}
